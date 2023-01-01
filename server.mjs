1764db12ebee424aafbc93c3cd7dd679fede8396
hasansheikh1 <hasanfaysal17@gmail.com>
2023-01-01 20:30:32 +0500

    some change

---
+++ server.mjs
@@ -0,0 +1,20 @@
+console.log("Server Mjs running");
+
+// const express = require('express')// The require keyword in this line of code is of old JS that's why iw ont work on express or mjs 
+import express from 'express';
+import path from 'path';
+const app = express()
+const port = 3000
+
+app.get('/abc', (req, res) => {
+
+    console.log("Request ip: ",req.ip);
+  res.send('Hello World!')
+})
+const __dirname = path.resolve()
+
+app.use('/', express.static(path.join(__dirname,'./web/build')))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
